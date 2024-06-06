e55b8711540301466c6c9e8c414a4c4dd53d8c73
qjq2126 <53382187+qjq2126@users.noreply.github.com>
2024-06-07 07:11:20 +0800

    更新 xingzhe.js

--- xingzhe.js
+++ xingzhe.js
@@ -1,23 +1,22 @@
 let body = $response.body;
 let obj = JSON.parse(body);
-if (!obj.code) {
-  obj.data.time_start = '1716568323000';
-  obj.data.time_end = '1719246723000';
-  obj.data.name = '太行';
-  obj.data.perms.workout_merge = '1';
-  obj.data.perms.map_skin = '2';
-  obj.data.perms.workout_merge = '1';
-  obj.data.perms.workout_map_hidden = '1';
-  obj.data.perms.pannel_object_2 = '1';
-  obj.data.perms.avatar = '1';
-  obj.data.perms.share_page = '1';
-  obj.data.perms.career_data = '1';
-  obj.data.perms.power_est = '1';
-  obj.data.perms.workout_rear = '1';
-  obj.data.perms.segment_contrast = '0';
-  obj.data.perms.pannel_object_5 = '0';
-  obj.data.perms.license_number_skin = '1';
-}
+obj.data.time_start = '1716568323000';
+obj.data.time_end = '1719246723000';
+obj.data.name = '太行';
+obj.data.perms.workout_merge = '1';
+obj.data.perms.map_skin = '2';
+obj.data.perms.workout_merge = '1';
+obj.data.perms.workout_map_hidden = '1';
+obj.data.perms.pannel_object_2 = '1';
+obj.data.perms.avatar = '1';
+obj.data.perms.share_page = '1';
+obj.data.perms.career_data = '1';
+obj.data.perms.power_est = '1';
+obj.data.perms.workout_rear = '1';
+obj.data.perms.segment_contrast = '0';
+obj.data.perms.pannel_object_5 = '0';
+obj.data.perms.license_number_skin = '1';
+
 
 body = JSON.stringify(obj);
-$done({body})
+$done({body});
