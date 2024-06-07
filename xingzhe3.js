5fd05bfa499c778e4b71a9d406cb8d98fd2269c0
qjq2126 <53382187+qjq2126@users.noreply.github.com>
2024-06-07 16:24:02 +0800

    更新 xingzhe3.js

--- xingzhe3.js
+++ xingzhe3.js
@@ -1,6 +1,6 @@
 let body = $response.body;
 let obj = JSON.parse(body);
-obj.data.pro_status.end_time = 1719246723000;
+obj.data.pro_status.end_time = '1719246723000';
 
 
 body = JSON.stringify(obj);
