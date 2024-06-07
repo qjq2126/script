d6e53e31e07d805ace4770bc06a841e2c12eab0d
qjq2126 <53382187+qjq2126@users.noreply.github.com>
2024-06-07 16:30:52 +0800

    更新 xingzhe.js

--- xingzhe.js
+++ xingzhe.js
@@ -19,6 +19,12 @@ obj.data.perms.license_number_skin = '1';
 
 obj.data.month_buy = '1';
 
+obj.data.benefit = '["HEAT算力增加5%", "运动会PRO赛事包"]';
+obj.data.benefit_can = '1';
+obj.data.time_start_visual = "2024-05-25T00:32:03.772816";
+obj.data.time_end_visual = "2024-06-25T00:32:03.772736";
+obj.data.perms_zh = '["地图皮肤0种", "轨迹合并", "隐藏地图", "PRO表盘1", "PRO会员头像框", "PRO会员分享页面", "生涯数据", "功率估算", "实景轨迹"]';
+
 
 body = JSON.stringify(obj);
 $done({body});
