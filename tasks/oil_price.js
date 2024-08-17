37eac0870dbba629fee743f003873a47000c499b
qjq2126 <53382187+qjq2126@users.noreply.github.com>
2024-08-17 08:37:31 +0800

    Create oil_price.js

---
+++ tasks/oil_price.js
@@ -0,0 +1,1 @@
+let url = 'https://www.z2zz.cn/api/today/oilpricev2?fromopenid=&openid=o5dT90N1AuG1r0cUzNuSti_toMPQ&city=%E9%99%95%E8%A5%BF&scene=1005&lat=34.16765462239584&lon=108.89948838975694&isIos=1'
