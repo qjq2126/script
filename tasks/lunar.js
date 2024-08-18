e79fb1bed6827d533d4ef8e2adb31b647f5b9053
qjq2126 <53382187+qjq2126@users.noreply.github.com>
2024-08-18 19:51:26 +0800

    Create lunar.js

---
+++ tasks/lunar.js
@@ -0,0 +1,11 @@
+let url = 'https://apis.tianapi.com/lunar/index?key=68383a430f95244ce89c46c0bb0d7d7c';
+if (typeof $task !== "undefined") {
+    $task.fetch(request).then(
+      function (response) {
+        $notify(response.body);
+      },
+      function (error) {
+        console.log(error);
+      }
+    );
+  }
