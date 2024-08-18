4bd99ac37b69edbe92c244216c06f5f35f7c2013
qjq2126 <53382187+qjq2126@users.noreply.github.com>
2024-08-18 19:53:38 +0800

    Update lunar.js

--- tasks/lunar.js
+++ tasks/lunar.js
@@ -1,6 +1,6 @@
 let url = 'https://apis.tianapi.com/lunar/index?key=68383a430f95244ce89c46c0bb0d7d7c';
 if (typeof $task !== "undefined") {
-    $task.fetch(request).then(
+    $task.fetch(url).then(
       function (response) {
         $notify(response.body);
       },
