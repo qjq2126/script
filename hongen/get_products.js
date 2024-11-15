beedac68075ca71de875f52355af5df3f7cfb6d2
qjq2126 <53382187+qjq2126@users.noreply.github.com>
2024-11-15 16:02:45 +0800

    修改文件
    修改文件

---
+++ hongen/get_products.js
@@ -0,0 +1,11 @@
+let body = $response.body;
+let obj = JSON.parse(body);
+
+obj.result.vip_info.vip_type = 2;
+obj.result.vip_info.expire_time = 1831846606;
+
+
+
+
+body = JSON.stringify(obj);
+$done({body});
