8fc2f869f79ca362ffc292cea5667f5aaee7787c
qjq2126 <53382187+qjq2126@users.noreply.github.com>
2024-11-15 15:55:20 +0800

    修改文件
    修改文件

--- hongen/get_user_rights.js
+++ hongen/get_user_rights.js
@@ -2,7 +2,7 @@ let body = $response.body;
 let obj = JSON.parse(body);
 
 obj.result.vip_info.vip_type = 2;
-obj.result.vip_info.expire_time = 1731846606;
+obj.result.vip_info.expire_time = 1831846606;
 
 
 body = JSON.stringify(obj);
