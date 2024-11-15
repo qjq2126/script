4145990cace19d0093d089a27170948430f30246
qjq2126 <53382187+qjq2126@users.noreply.github.com>
2024-11-15 13:30:15 +0800

    修改文件
    增加内容

--- hongen/check_vip.js
+++ hongen/check_vip.js
@@ -1,6 +1,9 @@
 let body = $response.body;
 let obj = JSON.parse(body);
-obj.result[0].
+obj.result[0].user_status_resp.effective_vip = true;
+obj.result[0].user_status_resp.l1_l3_package = false;
+obj.result[0].user_status_resp.l1_l9_package = false;
+obj.result[0].user_status_resp.l4_l9_package = false;
 
 body = JSON.stringify(obj);
 $done({body});
